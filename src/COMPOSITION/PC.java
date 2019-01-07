895338f9b56971fd8162cf965ef0beaba0c96cc1
Memnoc <smartdroidesign@gmail.com>
2019-01-07 16:05:53 +0000

    composition - PC example

---
+++ src/COMPOSITION/PC.java
@@ -0,0 +1,25 @@
+package COMPOSITION;
+
+public class PC {
+    private Case theCase;
+    private Monitor monitors;
+    private Motherboard motherboard;
+
+    public PC(Case theCase, Monitor monitors, Motherboard motherboard) {
+        this.theCase = theCase;
+        this.monitors = monitors;
+        this.motherboard = motherboard;
+    }
+
+    public Case getTheCase() {
+        return theCase;
+    }
+
+    public Monitor getMonitors() {
+        return monitors;
+    }
+
+    public Motherboard getMotherboard() {
+        return motherboard;
+    }
+}

ba8e8292496a2200fe71fabfdf457efa9cc10a6f
Memnoc <smartdroidesign@gmail.com>
2019-02-01 09:43:44 +0000

    Generics:
    Part 2 - implemented boundaries for Team types and universes

--- src/GENERICS/Main.java
+++ src/GENERICS/Main.java
@@ -4,15 +4,38 @@ public class Main {
 
     public static void main(String[] args) {
         Universe7 goku = new Universe7("Goku");
-        Universe6 kefla = new Universe6("Kefla");
-        Universe11 jiren = new Universe11("Jiren");
+        Universe7 vegeta = new Universe7("Vegeta");
+        Universe7 frieza = new Universe7("Frieza");
 
-        TournamentOfPower universe7 = new TournamentOfPower("Universe 7 - Team Goku");
+        Universe6 kefla = new Universe6("Kefla");
+        Universe6 hit = new Universe6("Hit");
+        Universe6 cabba = new Universe6("Cabba");
+
+        Universe11 jiren = new Universe11("Jiren");
+        Universe11 toppo = new Universe11("Toppo");
+        Universe11 dispo = new Universe11("Dispo");
+
+        TournamentOfPower<Universe7> universe7 = new TournamentOfPower<>("Universe 7 - Team Goku");
         universe7.addWarrior(goku);
-        universe7.addWarrior(kefla);
-        universe7.addWarrior(jiren);
+        universe7.addWarrior(vegeta);
+        universe7.addWarrior(frieza);
+        System.out.println("****************************************");
+
+        TournamentOfPower<Universe6> universe6 = new TournamentOfPower<>("Universe 6 - Team Hit");
+        universe6.addWarrior(kefla);
+        universe6.addWarrior(hit);
+        universe6.addWarrior(cabba);
+        System.out.println("*****************************************");
+
+        TournamentOfPower<Universe11> universe11 = new TournamentOfPower<>("Universe 11 - Team Toppo");
+        universe11.addWarrior(jiren);
+        universe11.addWarrior(toppo);
+        universe11.addWarrior(dispo);
+        System.out.println("******************************************");
 
         System.out.println(universe7.numWarriors());
+        System.out.println(universe6.numWarriors());
+        System.out.println(universe11.numWarriors());
 
     }
 }
--- src/GENERICS/TournamentOfPower.java
+++ src/GENERICS/TournamentOfPower.java
@@ -2,14 +2,14 @@ package GENERICS;
 
 import java.util.ArrayList;
 
-public class TournamentOfPower {
+public class TournamentOfPower<T extends Warrior> {
     private String teamName;
     int fight = 0;
     int won = 0;
     int lost = 0;
     int tied = 0;
 
-    private ArrayList<Warrior> members = new ArrayList<>();
+    private ArrayList<T> members = new ArrayList<>();
 
     public TournamentOfPower(String teamName) {
         this.teamName = teamName;
@@ -19,13 +19,13 @@ public class TournamentOfPower {
         return teamName;
     }
 
-    public boolean addWarrior(Warrior warrior) {
+    public boolean addWarrior(T warrior) {
         if(members.contains(warrior)) {
             System.out.println(warrior.getName() + " is already on the Team");
             return false;
         } else {
             members.add(warrior);
-            System.out.println(warrior.getName() + " picked for team " + this.teamName);
+            System.out.println(warrior.getName() + " picked for " + this.teamName);
             return true;
         }
     }
